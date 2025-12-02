163fd2166023b2271f663eb0e8070c2943eb4952
Sagar Dhawlapure <sdhawalapure@gmail.com>
2025-12-02 19:08:04 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (26.51%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.java
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -1,23 +1,40 @@
+// class Solution {
+//     public boolean isToeplitzMatrix(int[][] matrix) {
+//         int n=matrix.length;
+//         int m=matrix[0].length;
+//         HashMap<Integer,ArrayList<Integer>> mp=new HashMap<>();
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 int index=i-j;
+//                 mp.putIfAbsent(index,new ArrayList<>());
+//                 mp.get(index).add(matrix[i][j]);
+//             }
+//         }
+//         for(int key:mp.keySet()){
+//             ArrayList res=mp.get(key);
+//             int size=res.size();
+//             for(int i=0;i<size-1;i++){
+//                 if(res.get(i)!=res.get(i+1)){
+//                     return false;
+//                 }
+
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        int n=matrix.length;
-        int m=matrix[0].length;
-        HashMap<Integer,ArrayList<Integer>> mp=new HashMap<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                int index=i-j;
-                mp.putIfAbsent(index,new ArrayList<>());
-                mp.get(index).add(matrix[i][j]);
-            }
-        }
-        for(int key:mp.keySet()){
-            ArrayList res=mp.get(key);
-            int size=res.size();
-            for(int i=0;i<size-1;i++){
-                if(res.get(i)!=res.get(i+1)){
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] != matrix[i-1][j-1]) {
                     return false;
                 }
-
             }
         }
         return true;
