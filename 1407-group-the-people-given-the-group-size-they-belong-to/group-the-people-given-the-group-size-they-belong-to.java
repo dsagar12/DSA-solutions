18e15287037b66707e993ef30b9ff32d1ec63868
Sagar Dhawlapure <sdhawalapure@gmail.com>
2025-12-09 13:34:20 +0530

    Time: 8 ms (62.26%) | Memory: 47 MB (48.42%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
@@ -1,18 +1,21 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        HashMap<Integer,ArrayList<Integer>> mp=new HashMap<>();
-        List<List<Integer>> res=new ArrayList<>();
-        int n=groupSizes.length;
-        for(int i=0;i<n;i++){
-            mp.putIfAbsent(groupSizes[i],new ArrayList<>());
-            mp.get(groupSizes[i]).add(i);
-            if(groupSizes[i]==mp.get(groupSizes[i]).size()){
-                 res.add(new ArrayList<>(mp.get(groupSizes[i])));
 
-                mp.get(groupSizes[i]).clear();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < groupSizes.length; i++) {
+            int size = groupSizes[i];
+
+            map.putIfAbsent(size, new ArrayList<>());
+            map.get(size).add(i);
+
+            if (map.get(size).size() == size) {
+                res.add(map.get(size));
+                map.put(size, new ArrayList<>());
             }
         }
-       
+
         return res;
     }
 }
