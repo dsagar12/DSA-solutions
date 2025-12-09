c5e78bb698221039d02e0ceb8ea70090093a0d98
Sagar Dhawlapure <sdhawalapure@gmail.com>
2025-12-09 11:26:37 +0530

    Time: 58 ms (32.72%) | Memory: 58 MB (27.19%) - LeetSync

--- 2378-sender-with-largest-word-count/sender-with-largest-word-count.java
+++ 2378-sender-with-largest-word-count/sender-with-largest-word-count.java
@@ -1,26 +1,54 @@
+// class Solution {
+//     public String largestWordCount(String[] messages, String[] senders) {
+//         int n=senders.length;
+//         HashMap<String,Integer> mp=new HashMap<>();
+//         for(int i=0;i<n;i++){
+//             String s=messages[i];
+//             String[] sp=s.split(" ");
+//             int size=sp.length;
+//             mp.put(senders[i],mp.getOrDefault(senders[i],0)+size);
+//         }
+//        PriorityQueue<String> pq = new PriorityQueue<>(
+//     (a, b) -> {
+//         if (mp.get(a).equals(mp.get(b))) {
+//             return b.compareTo(a);   
+//         }
+//         return mp.get(b) - mp.get(a); 
+//     }
+// );
+
+
+//         for(String key:mp.keySet()){
+//             pq.offer(key);
+//         }
+//         return pq.poll();
+//     }
+// }
+
+
 class Solution {
     public String largestWordCount(String[] messages, String[] senders) {
-        int n=senders.length;
-        HashMap<String,Integer> mp=new HashMap<>();
-        for(int i=0;i<n;i++){
-            String s=messages[i];
-            String[] sp=s.split(" ");
-            int size=sp.length;
-            mp.put(senders[i],mp.getOrDefault(senders[i],0)+size);
-        }
-       PriorityQueue<String> pq = new PriorityQueue<>(
-    (a, b) -> {
-        if (mp.get(a).equals(mp.get(b))) {
-            return b.compareTo(a);   
-        }
-        return mp.get(b) - mp.get(a); 
-    }
-);
+        HashMap<String, Integer> mp = new HashMap<>();
 
-
-        for(String key:mp.keySet()){
-            pq.offer(key);
+        // 1️⃣ Count words per sender
+        for (int i = 0; i < senders.length; i++) {
+            int words = messages[i].split(" ").length;
+            mp.put(senders[i], mp.getOrDefault(senders[i], 0) + words);
         }
-        return pq.poll();
+
+        // 2️⃣ Find max sender
+        String ans = "";
+        int max = 0;
+
+        for (String sender : mp.keySet()) {
+            int count = mp.get(sender);
+
+            if (count > max || (count == max && sender.compareTo(ans) > 0)) {
+                max = count;
+                ans = sender;
+            }
+        }
+
+        return ans;
     }
 }
