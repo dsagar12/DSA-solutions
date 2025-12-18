c1ec6ff8e117fae69eb9c7e348c629a3ff4e04f3
Sagar Dhawlapure <sdhawalapure@gmail.com>
2025-12-18 13:34:30 +0530

    Time: 32 ms (26.89%) | Memory: 47.8 MB (46.06%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,33 +1,55 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(
+//             (a, b) -> {
+//                 int da = Math.abs(a - x);
+//                 int db = Math.abs(b - x);
+
+//                 if (da == db) {
+//                     return b - a; 
+//                 }
+//                 return db - da; 
+//             }
+//         );
+
+//         for (int num : arr) {
+//             pq.add(num);
+//             if (pq.size() > k) {
+//                 pq.poll();
+//             }
+//         }
+
+//         List<Integer> res = new ArrayList<>();
+//         while (!pq.isEmpty()) {
+//             res.add(pq.poll());
+//         }
+
+//         Collections.sort(res);
+//         return res;
+//     }
+// }
+
+
 
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>(
-            (a, b) -> {
-                int da = Math.abs(a - x);
-                int db = Math.abs(b - x);
-
-                if (da == db) {
-                    return b - a; 
-                }
-                return db - da; 
-            }
-        );
-
-        for (int num : arr) {
-            pq.add(num);
-            if (pq.size() > k) {
-                pq.poll();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int integer : arr) {
+            if (k > 0) {
+                minHeap.offer(integer);
+                k--;
+            } else if (Math.abs(minHeap.peek() - x) > Math.abs(integer - x)) {
+                minHeap.poll();
+                minHeap.offer(integer);
             }
         }
-
-        List<Integer> res = new ArrayList<>();
-        while (!pq.isEmpty()) {
-            res.add(pq.poll());
+        List<Integer> result = new ArrayList<>();
+        while (!minHeap.isEmpty()) {
+            result.add(minHeap.poll());
         }
-
-        Collections.sort(res);
-        return res;
+        return result;
     }
 }
